9b5104d48e7ad5474b5f3721b2e827e9ca89ae8b
Simon <simonho@cpp.edu>
2025-08-29 22:33:38 -0700

    changed it

--- github.java
+++ github.java
@@ -2,5 +2,6 @@ public class github {
 	public static void main(String[] args) {
 		System.out.println("Hello from the github class!");
         System.out.println("Hello Professor");
+        System.out.println("Changed this line");
     }
 }

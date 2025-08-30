336a97a5aaa5b463e9cb39affd9491fc9de79753
Simon <simonho@cpp.edu>
2025-08-29 22:01:14 -0700

    Added Javadoc and updated print message

--- github.java
+++ github.java
@@ -1,6 +1,6 @@
 public class github {
 	public static void main(String[] args) {
 		System.out.println("Hello from the github class!");
-        System.out.println("Hello Professor"
+        System.out.println("Hello Professor");
     }
 }
